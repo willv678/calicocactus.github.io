87b98509a71285fb8a04fa301dd04826355f723e
Will <willv678@gmail.com>
2022-10-20 15:05:40 -0400

    :+1:

--- script.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-function newLink(){
-    let link = "https://ebird.org/checklist/S"
-    let end = Math.floor(Math.random() * (90000000 - 10000000)) + 10000000;
-    let final = link + end
-    window.open(final);
-}
