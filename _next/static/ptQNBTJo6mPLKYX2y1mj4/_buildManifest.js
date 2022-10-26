212f9e41588b8040a7bade860e1d605bbbe60593
Will <willv678@gmail.com>
2022-10-25 22:53:04 -0400

    Components!

---
+++ _next/static/ptQNBTJo6mPLKYX2y1mj4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/5a722ef2f8831d06.css","static/chunks/pages/index-7c8a9c19ad640cc8.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
