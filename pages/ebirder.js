40f3929c36b16ceacdab30778affbb07d9467194
Will <willv678@gmail.com>
2023-02-06 18:23:11 -0500

    center lol

--- pages/ebirder.js
+++ pages/ebirder.js
@@ -1,9 +1,11 @@
 export default function EBirder() {
     return (
-     <>
-      <h1>EBirder</h1>
-      <h3>Browse a random Ebird.org list and explore</h3>
-      <button onClick={() => window.open("https://ebird.org/checklist" + randomLink())}>Visit Site</button>
+     <><center>
+        <h1>EBirder</h1>
+        <h3>Browse a random Ebird.org list and explore</h3>
+        <button onClick={() => window.open("https://ebird.org/checklist" + randomLink())}>Visit Site</button>
+     </center>
+
      </>
     )
   }
