0efcd79e3d8b4d000fc7f5eace78a50bdd87117e
Will Varner <willv678@gmail.com>
2025-03-27 13:03:41 -0400

    delusion

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  output: 'export',
+  basePath: isProd ? '/calicocactus.github.io' : '',
   images: {
     unoptimized: true,
   },
-  
 }
-
-module.exports = nextConfig
