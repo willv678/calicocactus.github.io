746d261e6b99046438d1693b3f5e41ed7f334e5f
Will Varner <willv678@gmail.com>
2025-03-27 12:43:44 -0400

    maybe fix ?

--- components/TopBar.js
+++ components/TopBar.js
@@ -5,6 +5,7 @@ export default function TopBar(){
     return  <div className={styles.topBar}>
                 <TopBarItem destination="https://willvarner.me" text="Home"/>
                 <TopBarItem destination="https://willvarner.me/#projects" text="My Work"/>
-                <TopBarItem destination="https://www.linkedin.com/in/will-varner/" text="Resume"/>
+                <TopBarItem destination="https://www.linkedin.com/in/will-varner/" text="LinkedIn"/>
+                <TopBarItem destination="/csresume.pdf" text="Resume"/>
             </div>
 }
