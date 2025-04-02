092d13db654162352f638843051d21ae80112577
Will Varner <willv678@gmail.com>
2025-04-02 18:44:29 -0400

    Update AboutMe.js
    updated new description

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -1,5 +1,5 @@
 import styles from '../styles/Home.module.css'
 
 export default function AboutMe() {
-    return <p className={styles.aboutMe}>Hi there! My name&lsquo;s Will, a student developer with a passion for all things tech. I love working with an array of things, from learning a new programming language to 3D-printing a wireless 3-in-one charger. This site acts as my hub for storing all my works, from software development to video production, everything Will can be found here, or open source on my Github Page.</p>
+    return <p className={styles.aboutMe}>Hi there! My namee&lsquo;s Will, a student developer with a passion for all things tech. I love working on all sorts of things, from learning a new programming language to developing self-driving logic in in UGAe&lsquo;s MSC lab. This site is my hub for storing all my works, from software development to video editing, everything Will can be found here, or open source on my Github!</p>
 }
