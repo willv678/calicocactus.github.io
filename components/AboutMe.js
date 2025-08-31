4463e71ee32b819ff17036d1e54906496cf8f0c1
Will Varner <willv678@gmail.com>
2025-08-31 14:10:18 -0400

    in fix

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -1,5 +1,5 @@
 import styles from '../styles/Home.module.css'
 
 export default function AboutMe() {
-    return <p className={styles.aboutMe}>Hi there! My name&lsquo;s Will, a student developer with a passion for all things tech. I love working on all sorts of things, from learning a new programming language to developing self-driving logic in in UGA&lsquo;s MSC lab. This site is my hub for storing all my works, from software development to video editing, everything Will can be found here, or open source on my Github!</p>
+    return <p className={styles.aboutMe}>Hi there! My name&lsquo;s Will, a student developer with a passion for all things tech. I love working on all sorts of things, from learning a new programming language to developing self-driving logic in UGA&lsquo;s MSC lab. This site is my hub for storing all my works, from software development to video editing, everything Will can be found here, or open source on my Github!</p>
 }
