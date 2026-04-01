9a4ce4749cbb34574bf48a57f5d5753a8f8b15d1
Will Varner <willv678@gmail.com>
2026-04-01 16:24:23 -0400

    Google analytics ding

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
+import Script from 'next/script'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-1LS1VWVKVB"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-1LS1VWVKVB');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
